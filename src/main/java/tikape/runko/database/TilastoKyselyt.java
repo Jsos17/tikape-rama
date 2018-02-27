71f6dabcce3821e9410fbdddb23e798b11dbecea
Niskala Iiro H k <iniskala@da2-chemb134b-3.it.helsinki.fi>
2018-02-27 14:45:01 +0200

    Tilasto huubaa

--- src/main/java/tikape/runko/database/TilastoKyselyt.java
+++ src/main/java/tikape/runko/database/TilastoKyselyt.java
@@ -26,8 +26,8 @@ public class TilastoKyselyt {
         int monessa = 0;
         
         try (Connection conn = this.db.getConnection(); 
-                PreparedStatement stmt = conn.prepareStatement("SELECT COUNT(*) FROM SmoothieRaakaAine, RaakaAine "
-                            + "WHERE SmoothieRaakaAine.raaka_aine_id = RaakaAine.id AND RaakaAine.nimi = ?")) {
+                PreparedStatement stmt = conn.prepareStatement("SELECT COUNT (DISTINCT Smoothie.nimi) FROM Smoothie, SmoothieRaakaAine, RaakaAine " 
+                        + "WHERE RaakaAine.nimi = ? AND SmoothieRaakaAine.raaka_aine_id = RaakaAine.id AND SmoothieRaakaAine.smoothie_id = Smoothie.id;"))  {
             stmt.setString(1, raaka_aine_nimi);
             
             ResultSet rs =  stmt.executeQuery();
