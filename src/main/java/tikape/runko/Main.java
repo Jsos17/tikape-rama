befdaec01bb272a7f949cf462ea499ce431e7c32
Silvennoinen Juuso P S <jpssilve@lx7-fuxi345.ad.helsinki.fi>
2018-02-28 14:18:18 +0200

    pom muokatttu, mainia muokattu

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -23,9 +23,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         
-//        if (System.getenv("PORT") != null) {
-//            Spark.port(Integer.valueOf(System.getenv("PORT")));
-//        }
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
         
         Database database = new Database("jdbc:sqlite:smoothiet.db");
         database.init();
@@ -185,12 +185,12 @@ public class Main {
         });      
     }
     
-//    public static Connection getConnection() throws Exception {
-//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-//        if (dbUrl != null && dbUrl.length() > 0) {
-//            return DriverManager.getConnection(dbUrl);
-//        }
-//
-//        return DriverManager.getConnection("jdbc:sqlite:huonekalut.db");
-//    }
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
+        return DriverManager.getConnection("jdbc:sqlite:smoothiet.db");
+    }
 }
